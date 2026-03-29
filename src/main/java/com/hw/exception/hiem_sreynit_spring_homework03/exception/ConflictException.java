c996b71810e23fee489b169302e9d0788c8773e9
Hiem-Sreynit <hiemsreynit282@gmail.com>
2026-03-29 17:46:18 +0700

    update: added event controller with get all events, get event by id, and create event operations.

--- src/main/java/com/hw/exception/hiem_sreynit_spring_homework03/exception/ConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.hw.exception.hiem_sreynit_spring_homework03.exception;
-
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}
